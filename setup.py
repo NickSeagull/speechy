5b0727a602df2610b191b4b1a2920e51124047ab
Nikita Tchayka <nikitatchayka@gmail.com>
2016-03-12 21:43:53 +0000

    Add basic info for PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'speechy',
+    packages = ['speechy'],
+    install_requires = ['hy>=0.10.0'],
+    version = '0.1',
+    description = 'A TDD/BDD framework for Hy',
+    author = 'Nikita Tchayka',
+    author_email = 'nikitatchayka@gmail.com',
+    url = 'https://github.com/nickseagull/speechy',
+    download_url = 'https://github.com/nickseagull/speechy/tarball/0.1',
+    keywords = ['testing', 'tdd', 'bdd', 'spec', 'hy', 'hylang'],
+    classifiers = [],
+)
