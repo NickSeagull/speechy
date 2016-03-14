4a0f4eea7d63f832d34885851a778e778c864c44
Nikita Tchayka <nikitatchayka@gmail.com>
2016-03-14 16:05:32 +0000

    Add classifiers

--- setup.py
+++ setup.py
@@ -14,5 +14,16 @@ setup(
     url = 'https://github.com/nickseagull/speechy',
     download_url = 'https://github.com/nickseagull/speechy/tarball/0.1',
     keywords = ['testing', 'tdd', 'bdd', 'spec', 'hy', 'hylang'],
-    classifiers = [],
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+    ],
 )
